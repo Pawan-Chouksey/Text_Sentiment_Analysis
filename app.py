ab7be3031b2e0a96dbb4fe94acd7bb139dd4e658
Pawan-Chouksey <121729899+Pawan-Chouksey@users.noreply.github.com>
2026-05-08 02:46:55 +0530

    Migrate Flask app to FastAPI framework

--- app.py
+++ app.py
@@ -1,34 +1,50 @@
-from flask import Flask, request, render_template, send_file
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
 import pickle
 
+app = FastAPI()
 
-TSA_Model= Flask(__name__)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-
+# Load model
 with open('model/model.pkl', 'rb') as file:
     model = pickle.load(file)
 
+# Load vectorizer
 with open('model/vectorizer.pkl', 'rb') as file:
     vectorizer = pickle.load(file)
 
 
-@TSA_Model.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "index.html"
+    )
 
 
-@TSA_Model.route('/predict', methods=['POST'])
-def prediction():
-    if request.method == 'POST':
-        user_input = request.form.get('text')
-        if user_input: 
-            transformed_input = vectorizer.transform([user_input])
-            predicted_sentiment = model.predict(transformed_input)
-            result = predicted_sentiment[0]
+@app.post("/predict", response_class=HTMLResponse)
+async def prediction(request: Request, text: str = Form(...)):
 
-            return render_template('result.html', result=result)
-        else:
-            return "No input provided"    
+    if text:
+        transformed_input = vectorizer.transform([text])
+        predicted_sentiment = model.predict(transformed_input)
+        result = predicted_sentiment[0]
 
-if __name__ == '__main__':
-    TSA_Model.run(host='0.0.0.0', port=5000)
+        return templates.TemplateResponse(
+            request,
+            "result.html",
+            {
+                "result": result
+            }
+        )
+
+    return HTMLResponse(content="No input provided")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
