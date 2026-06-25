3abc23c1357f803f8ee31bf9230b1b986da19cf4
Pawan-Chouksey <pawanchouksey636@gmail.com>
2026-06-25 20:47:52 +0530

    Optimized

--- BackEnd/TSA_image.py
+++ BackEnd/TSA_image.py
@@ -1,10 +1,15 @@
 import cv2 as cv
 from easyocr import Reader
 
-reader = Reader(['en'])
+reader = None
+
+def get_reader():
+    global reader
+    if reader is None:
+        reader = Reader(['en'])
+    return reader
 
 def extract(image_path: str):
-
     img = cv.imread(image_path)
 
     if img is None:
@@ -12,8 +17,13 @@ def extract(image_path: str):
 
     grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     blur = cv.GaussianBlur(grey, (5,5), 0)
-    _, thres = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    _, thres = cv.threshold(
+        blur,
+        0,
+        255,
+        cv.THRESH_BINARY + cv.THRESH_OTSU
+    )
 
-    text = reader.readtext(thres, detail=0)
+    text = get_reader().readtext(thres, detail=0)
 
     return " ".join(text).strip()
